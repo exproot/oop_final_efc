9b1ecd686b2a4cf61442ddd9ebd8205d078211dd
Ertan Yagmur <ertanyagmur@outlook.com>
2023-01-01 15:39:06 +0300

    prop

---
+++ efc_console/Entities/Book.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace efc_console.Entities {
+    public class Book {
+        [Key]
+        public int BookId { get; set; }
+        [Required]
+        [MaxLength(50)]
+        public string BookTitle { get; set; }
+    }
+}
